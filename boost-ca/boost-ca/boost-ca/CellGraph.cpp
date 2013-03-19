57bc2c2f5a9247e22cdbdc297984faf4e85cf666
Kirill Romanovskiy <romanovskiy.k@gmail.com>
2013-03-19 21:19:45 +0400

    add CellGraph class

--- boost-ca/boost-ca/boost-ca/CellGraph.cpp
+++ boost-ca/boost-ca/boost-ca/CellGraph.cpp
@@ -7,3 +7,108 @@
 //
 
 #include "CellGraph.h"
+
+#include <algorithm>
+#include <fstream>
+#include <string>
+#include <iostream>
+#include <list>
+
+#include <boost/lexical_cast.hpp>
+#include <boost/foreach.hpp>
+#include <boost/algorithm/string.hpp>
+#include <boost/graph/adjacency_iterator.hpp>
+
+using namespace boost::graph;
+using boost::lexical_cast;
+using boost::property_map;
+using boost::tie;
+using boost::vertex_index;
+
+using std::cout;
+using std::endl;
+using std::ifstream;
+using std::list;
+using std::string;
+using std::vector;
+
+CellGraph::CellGraph(size_t vertexCount):m_graph(new graph_t(vertexCount))
+{
+    // TODO: add correct method to fill the cells
+    CellIterator vi, viend;
+    CellValue value = get(&VertexProperties::cellValue, *(m_graph.get()));
+
+    for (tie(vi,viend) = vertices(*m_graph.get()); vi != viend; ++vi)
+        value[*vi] = rand()%2;
+}
+
+void CellGraph::printState()
+{
+    boost::print_vertices(*m_graph.get(), get(&VertexProperties::cellValue, *(m_graph.get())));
+}
+
+void CellGraph::printEdges()
+{
+    boost::print_edges(*m_graph.get(), get(vertex_index, *m_graph.get()));
+}
+
+GraphState CellGraph::getCurrentState()
+{
+    CellIterator vi, viend;
+    CellValue value = get(&VertexProperties::cellValue, *(m_graph.get()));
+    GraphState state;
+    state.clear();
+    
+    for (tie(vi,viend) = vertices(*m_graph.get()); vi != viend; ++vi)
+        state.push_back(value[*vi]);
+    return state;
+}
+
+void CellGraph::evolve()
+{
+    CellValue value = get(&VertexProperties::cellValue, *m_graph.get());
+    CellIterator vi, vi_end;
+    graph_t::adjacency_iterator ni, ni_end;
+    
+    bool* vars = new bool[edgeCount];
+    for (tie(vi,vi_end) = vertices(*m_graph.get()); vi != vi_end; ++vi)
+    {
+        int i = 0;
+        for (tie(ni, ni_end) = adjacent_vertices(*vi, *m_graph.get()); ni != ni_end; ++ni)
+        {
+            vars[i] = (value[*ni]);
+            ++i;
+        }
+        value[*vi] = localLinkFunction(edgeCount, vars);
+    }
+    delete [] vars;
+}
+
+void CellGraph::loadFromFile(const string &path)
+{
+    ifstream file(path);
+    int currentVertexIndex = 0;
+    unsigned int lastEdgeCount = 0;
+    while (file.peek() != EOF)
+    {
+        string adjacencyString;
+        getline(file, adjacencyString);
+        if (adjacencyString.empty()) continue;
+        
+        list<string> vertexStringList;
+        boost::split(vertexStringList, adjacencyString, boost::is_any_of(","), boost::token_compress_on);
+        edgeCount = static_cast<unsigned int>(vertexStringList.size());
+        // TODO: create exceptions
+        if (lastEdgeCount != 0 && lastEdgeCount != edgeCount)
+            throw "Edge count differs!";
+        lastEdgeCount = edgeCount;
+        BOOST_FOREACH(string vertexString, vertexStringList)
+        {
+            int index = lexical_cast<int>(vertexString);
+            add_edge(vertex(currentVertexIndex, *m_graph.get()), vertex(index, *m_graph.get()), *m_graph.get());
+        }
+        vertexStringList.clear();
+        currentVertexIndex++;
+    }
+}
+
--- boost-ca/boost-ca/boost-ca/CellGraph.h
+++ boost-ca/boost-ca/boost-ca/CellGraph.h
@@ -10,5 +10,48 @@
 #define __boost_ca__CellGraph__
 
 #include <iostream>
+#include <vector>
+
+#include <boost/config.hpp>
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/graph_utility.hpp>
+#include <boost/property_map/property_map.hpp>
+#include <boost/scoped_ptr.hpp>
+
+struct VertexProperties
+{
+    // TODO: get rid of index property
+    // see http://www.boost.org/doc/libs/1_42_0/libs/graph/test/layout_test.cpp
+    bool cellValue;
+};
+
+typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::directedS, VertexProperties> graph_t;
+typedef boost::property_map<graph_t, boost::vertex_index_t>::type CellIndex;
+typedef boost::property_map<graph_t, bool VertexProperties::*>::type CellValue;
+typedef boost::graph_traits<graph_t>::vertex_iterator CellIterator;
+typedef boost::graph_traits<graph_t>::out_edge_iterator OutEdgeIterator;
+typedef boost::graph_traits<graph_t>::in_edge_iterator InEdgeIterator;
+typedef std::vector<bool> GraphState;
+
+class CellGraph
+{
+    boost::scoped_ptr<graph_t> m_graph;
+    unsigned int edgeCount;
+    bool localLinkFunction(int argc, const bool* argv)
+    {
+        return (argv[0] + argv[1]) % 2;
+    }
+
+public:
+    CellGraph(size_t vertexCount);
+
+    GraphState getCurrentState();
+    void evolve();
+    
+    void loadFromFile(const std::string& path);
+
+    void printState();
+    void printEdges();
+};
 
 #endif /* defined(__boost_ca__CellGraph__) */
